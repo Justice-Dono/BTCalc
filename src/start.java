dad3d41c97dd8764f130c6bf85f7704ddd7c4703
Justice-Dono <jordan.tewnion@gmail.com>
2024-01-14 15:02:51 -0700

    Hardcoded in tribes

--- src/start.java
+++ src/start.java
@@ -1,6 +1,23 @@
 public class start {
     public static void main(String[] args) throws Exception {
+        tribe Amaljaa = tribe.oldTribe(1,3,10,14,20,0,0,0,0);
+        tribe Kobold = tribe.oldTribe(1,3,10,14,20,0,0,0,0);
+        tribe Sahagin = tribe.oldTribe(1,3,10,14,20,0,0,0,0);
+        tribe Sylph = tribe.oldTribe(1,3,10,14,20,0,0,0,0);
+        tribe Ixali = tribe.oldTribe(1,6,20,24,29,35,42,50,0);
+        tribe VanuVanu = tribe.oldTribe(1,7,50,50,50,50,50,50,50);
+        tribe Moogle = tribe.oldTribe(1,7,50,50,50,50,50,50,50);
+        tribe Vath = tribe.oldTribe(3,7,0,0,70,70,70,70,70);
+        tribe Namazu = new tribe(3, 7);
+        tribe Anata = new tribe(3, 7);
+        tribe Kojin = new tribe(3, 7);
+        tribe Pixie = new tribe(3, 7);
+        tribe Qitari = new tribe(3, 7);
+        tribe Dwarf = new tribe(3, 7);
+        tribe Arkasodara = new tribe(3, 7);
         tribe Omicron = new tribe(3, 7);
+        tribe Loporrit = new tribe(3, 7);
         System.out.println(Omicron.rankCalc(3));
+        System.out.println(Vath.rankCalc(5));
     }
 }
--- src/tribe.java
+++ src/tribe.java
@@ -2,19 +2,19 @@ public class tribe {
     private static final Integer[] rankTable = {0,150,360,510,720,990,1320,1730};
     public static Integer startRank;
     public static Integer maxRank;
-    public static Integer r1Xp;
-    public static Integer r2Xp;
-    public static Integer r3Xp;
-    public static Integer r4Xp;
-    public static Integer r5Xp;
-    public static Integer r6Xp;
-    public static Integer r7Xp;
     public Integer[] xpTable;
 
     public tribe(Integer start, Integer max){
         startRank = start;
         maxRank = max;
         xpTable = new Integer[] {0,0,0,60,60,60,60,60};
+    }
+
+    public static tribe oldTribe(Integer start, Integer max, Integer r1, Integer r2, Integer r3, Integer r4, Integer r5, Integer r6, Integer r7){
+        tribe localTribe = new tribe(start, max);
+
+        localTribe.xpTable = new Integer[] {0, r1, r2, r3, r4, r5, r6, r7};
+        return localTribe;
     }
 
     public int rankCalc(Integer rank){
